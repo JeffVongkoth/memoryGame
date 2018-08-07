9dfad607277dee33fcc94ef4d68a1be5e5757834
theRealGerph <jvongkoth@gmail.com>
2018-08-07 14:54:45 -0500

    fixed images and header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
+import Header from "./components/Header";
 import friends from "./friends.json";
 import "./App.css";
 
@@ -22,16 +22,13 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Header>Clicky Game</Header>
         {this.state.friends.map(friend => (
           <FriendCard
             removeFriend={this.removeFriend}
             id={friend.id}
             key={friend.id}
-            name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
           />
         ))}
       </Wrapper>
